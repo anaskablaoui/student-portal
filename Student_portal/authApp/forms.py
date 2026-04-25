22a8cd0ed73706604ef919ea09da1c575f065a77
anas <anas.kablaoui@gmail.com>
2026-04-25 23:46:41 +0100

    partie login

--- Student_portal/authApp/forms.py
+++ Student_portal/authApp/forms.py
@@ -5,17 +5,36 @@ from Etudiant.models import Etudiant
 
 
 class ProfesseurLoginForm(forms.ModelForm):
-    matricule=forms.CharField(widget=forms.TextInput,label="type matricule")
-    password=forms.CharField(widget=forms.PasswordInput,label="type password")
+    matricule=forms.CharField(widget=forms.TextInput(attrs={
+                            'class':'input',
+                            'placeholder':'entrer votre matricule'
+                        }),
+                              label="Matricule"
+                              
+                              )
+    password=forms.CharField(widget=forms.PasswordInput(attrs={
+                            'class':'input',
+                            'placeholder':'entrer votre mot de passe '
+                        }),
+                             label="type password"
+                             )
     
     class Meta:
         model= Professeur
         fields =['matricule','password']
         
 class EtudiantLoginForm(forms.ModelForm):
-    matricule=forms.CharField(widget=forms.TextInput,label='type matricule')
-    password=forms.CharField(widget=forms.PasswordInput,label='type password')
+    matricule=forms.CharField(widget=forms.TextInput(attrs={
+                            'class':'input',
+                            'placeholder':'entrer votre matricule'
+                        }),
+                              label='type matricule')
+    password=forms.CharField(widget=forms.PasswordInput(attrs={
+                            'class':'input',
+                            'placeholder':'entrer votre mot de passe '
+                        }),
+                             label='type password')
     
     class Meta:
-        model= Professeur
+        model= Etudiant
         fields =['matricule','password']
