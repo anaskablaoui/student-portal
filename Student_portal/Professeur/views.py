2305abe3d2f4c2cf0c556066b06fcd8b5f5eab34
Oumaima El hachami <oumaima.oumaimael2005@gmail.com>
2026-04-26 20:21:36 +0100

    fixing bugs

--- Student_portal/Professeur/views.py
+++ Student_portal/Professeur/views.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Professeur, Note, Rapport, presence
-
-@login_required(login_url='/login/')
-def professeur_dashboard(request):
-    # request.user contient le professeur connecté
-    professeur = request.user
-    
-    # récupère les rapports que ce professeur a créé
-    rapports = Rapport.objects.filter(professeur=professeur)
-    
-    # récupère les notes que ce professeur a donné
-    notes = Note.objects.filter(matiere__in=request.user.matieres.all()) \
-            if hasattr(request.user, 'matieres') else []
-
-    return render(request, 'professeur/index.html', {
-        'professeur': professeur,
-        'rapports': rapports,
-    })
-=======
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RapportForm
@@ -41,4 +19,4 @@ def rediger_rapport(request):
         form = RapportForm(professeur=professeur)
 
     return render(request, 'professeur/rapport_form.html', {'form': form})
->>>>>>> bc472ec5fdec734df3aae065de484c5e2f6d07b2
+
